ac250fa914663eae1e4593f0da7da21e0e03c398
nomadbard916 <neonnova916@gmail.com>
2021-08-21 21:59:34 +0800

    versioning

---
+++ routes/v2.py
@@ -0,0 +1,10 @@
+from models.user import User
+from fastapi import FastAPI, Header
+from starlette.status import HTTP_201_CREATED
+
+app_v2 = FastAPI(openapi_prefix="/v2")
+
+
+@app_v2.post("/user", status_code=HTTP_201_CREATED)
+async def post_user(user: User, x_custom: str = Header(...)):
+    return {"request body": "it is version 2"}
--- run.py
+++ run.py
@@ -1,55 +1,8 @@
-from models.book import Book
-from models.user import User
-from models.author import Author
 from fastapi import FastAPI, Body, Header, File
-from starlette.status import HTTP_201_CREATED
-from starlette.responses import Response
+from routes.v1 import app_v1
+from routes.v2 import app_v2
 
 app = FastAPI()
 
-
-@app.get("/user", status_code=HTTP_201_CREATED)
-async def post_user(user: User, x_custom: str = Header(...)):
-    return {"request body": user, "request custom headers": x_custom}
-
-
-@app.get("/user")
-async def get_user_validation(password: str):
-    return {"query parameter": password}
-
-
-@app.get("/book/{isbn}", response_model=Book, response_model_exclude=["author"])
-async def get_book_with_isbn(isbn: str):
-    author_dict = {"name": "author1", "book": ["books", "book2"]}
-    author1 = Author(**author_dict)
-
-    book_dict = {"isbn": "isbn1", "name": "book1", "year": 2019, "author": author1}
-    book1 = Book(**book_dict)
-
-    return book1
-
-
-@app.get("/author/{id}/book")
-async def get_authors_books(id: int, category: str, order: str = "asc"):
-    return {"query changable parameter": order + category + str(id)}
-
-
-@app.patch("/author/name")
-async def patch_author_name(name: str = Body(..., embed=True)):
-    return {"name in body": name}
-
-
-@app.post("/user/author")
-async def post_user_and_author(
-    user: User, author: Author, bookstore_name: str = Body(..., embed=True)
-):
-
-    return {"user": user, "author": author, "bookstore": bookstore_name}
-
-
-@app.post("/user/photo")
-async def upload_user_photo(response: Response, profile_photo: bytes = File(...)):
-    response.headers["x-file-size"] = str(len(profile_photo))
-    response.set_cookie(key="cookie-api", value="test")
-
-    return {"file size": len(profile_photo)}
+app.mount("/v1/", app_v1)
+app.mount("/v2/", app_v2)
