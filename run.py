258ab7565c7b6a98dd8090df6cf02af3cdbf6d48
nomadbard916 <neonnova916@gmail.com>
2021-08-21 21:36:55 +0800

    advanced endpoint issues

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from models.book import Book
 from models.user import User
 from models.author import Author
-from fastapi import FastAPI, Body, Header
+from fastapi import FastAPI, Body, Header, File
+from starlette.status import HTTP_201_CREATED
+from starlette.responses import Response
 
 app = FastAPI()
 
 
-@app.get("/user")
+@app.get("/user", status_code=HTTP_201_CREATED)
 async def post_user(user: User, x_custom: str = Header(...)):
     return {"request body": user, "request custom headers": x_custom}
 
@@ -16,9 +18,15 @@ async def get_user_validation(password: str):
     return {"query parameter": password}
 
 
-@app.get("/book/{isbn}", response_model=Book)
+@app.get("/book/{isbn}", response_model=Book, response_model_exclude=["author"])
 async def get_book_with_isbn(isbn: str):
-    return {"query changable parameter": isbn}
+    author_dict = {"name": "author1", "book": ["books", "book2"]}
+    author1 = Author(**author_dict)
+
+    book_dict = {"isbn": "isbn1", "name": "book1", "year": 2019, "author": author1}
+    book1 = Book(**book_dict)
+
+    return book1
 
 
 @app.get("/author/{id}/book")
@@ -37,3 +45,11 @@ async def post_user_and_author(
 ):
 
     return {"user": user, "author": author, "bookstore": bookstore_name}
+
+
+@app.post("/user/photo")
+async def upload_user_photo(response: Response, profile_photo: bytes = File(...)):
+    response.headers["x-file-size"] = str(len(profile_photo))
+    response.set_cookie(key="cookie-api", value="test")
+
+    return {"file size": len(profile_photo)}
