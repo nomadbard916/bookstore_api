fa795cd8f97a1e73a28e4572afc73e5e04fd2c71
nomadbard916 <neonnova916@gmail.com>
2021-08-28 21:40:25 +0800

    implement sql queries

---
+++ utils/db.py
@@ -0,0 +1,46 @@
+from databases import Database
+from utils.const import DB_URL
+
+
+async def connect_db():
+    db = Database(DB_URL)
+    await db.connect()
+    return db
+
+
+async def disconnect_db(db):
+    await db.disconnect()
+
+
+async def execute(query, is_many, values=None):
+    db = await connect_db()
+
+    if is_many:
+        await db.execute_many(query=query, values=values)
+    else:
+        await db.execute(query=query, values=values)
+
+    await disconnect_db(db)
+
+
+async def fetch(query, is_one, values=None):
+    db = await connect_db()
+
+    if is_one:
+        result = await db.fetch_one(query=query, values=values)
+        if result is None:
+            out = None
+        else:
+            out = dict(result)
+    else:
+        result = await db.fetch_all(query=query, values=values)
+        if result is None:
+            out = None
+        else:
+            out = []
+            for row in result:
+                out.append(dict(row))
+
+    await disconnect_db(db)
+
+    return out
